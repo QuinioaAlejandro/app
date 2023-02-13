1601ac46c18df6a271720a697fda1f5dd59c64c1
QuinioaAlejandro <alejandroquinioa@gmail.com>
2023-02-12 23:13:05 -0300

    entregafinal

--- README.md
+++ README.md
@@ -70,3 +70,9 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
 
 
+# quinoto records
+ Quinoto records un simulador ecommerce de venta de discos de vinilos.
+## librerias usadas 
+sweet alert
+
+## https://appef.vercel.app/
--- src/components/CartList.js
+++ src/components/CartList.js
@@ -6,11 +6,13 @@ const CartList = ({carrito}) => {
 
 const {vaciarCarrito, precioTotal} = useCarrito()
 return(
-<div> 
-    <button onClick={vaciarCarrito}>Vaciar Carrito</button>
+<div className="cart-checkout"> 
+    <div className="cart-checkout__text">
     {carrito.map((el)=> (
     <CartItem key={el.id} id={el.id} nombre={el.nombre} cantidad={el.cantidad} precio={el.precio} imagen={el.imagen}/>
-))} subtotal:${precioTotal} </div>
+    ))} subtotal:${precioTotal} </div>
+    <button className="cart-checkout__button" onClick={vaciarCarrito}><img src="../img/vaciar-carrito.png" alt="vaciar carrito" /></button>
+</div>
 )
 }
 
